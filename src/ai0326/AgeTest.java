e6cae58eab884fbf4c7b192b36db33a7380d4b04
kjminn <kjminn2001@gmail.com>
2026-04-16 10:46:33 +0900

    Java 1 ~ 4장

--- src/ai0326/AgeTest.java
+++ src/ai0326/AgeTest.java
@@ -1,14 +1,21 @@
 package ai0326;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
-public class ScannerTest01 {
+public class AgeTest {
     public static void main(String[] args) {
+        System.out.println("======== 나이 계산 ========");
         Scanner s = new Scanner(System.in);
-        System.out.print("좋아하는 정수값은 입력하세요.:");
-        int num = s.nextInt();
+        System.out.print("* 출생년도를 입력하세요:");
+        int birthYear = s.nextInt();
+//        현재년도 구하기
+        Calendar calendar = Calendar.getInstance();
+        int nowYear = calendar.get(Calendar.YEAR);
 
-        System.out.println("입력한 숫자는 "+ num +"입니다.");
+        int age = nowYear - birthYear;
+
+        System.out.println("당신의 현재 나이는 "+ age +"세 입니다.");
         s.close();
     }
 }
